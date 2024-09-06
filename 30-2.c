8f3300a65249d22271bff754846ba4a68eadffe7
dayy <1154955681@qq.com>
2024-09-06 22:56:42 +0800

    change 30-2.c

--- 30-2.c
+++ 30-2.c
@@ -22,6 +22,7 @@ int get()
     return buffer;
 }
 
+/* first try, but has problem when has two consumer
 void *producer(void *arg)
 {
     int i;
@@ -52,4 +53,36 @@ void *consumer(void *arg)
         printf("%d\n", tmp);
     }
 }
+*/
 
+/* second try, replace if by while */
+void *producer(void *arg)
+{
+    int i;
+    int loops = (int)arg;
+    for (i = 0; i < loops; i++)
+    {
+        pthread_mutex_lock(&mutex);                         //p1
+        while (count == 1)                                  //p2
+            pthread_cond_wait(&cond, &mutex);               //p3
+        put(i);                                             //p4
+        pthread_cond_signal(&cond);                         //p5
+        pthread_mutex_unlock(&mutex);                       //p6
+    }
+}
+
+void *consumer(void *arg)
+{
+    int i;
+    int loops = (int)arg;
+    for (i = 0; i < loops; i++)
+    {
+        pthread_mutex_lock(&mutex);                         //c1
+        while (count == 0)                                  //c2
+            pthread_cond_wait(&cond, &mutex);               //c3
+        int tmp = get();                                    //c4
+        pthread_cond_signal(&cond);                         //c5
+        pthread_mutex_unlock(&mutex);                       //c6
+        printf("%d\n", tmp);
+    }
+}
